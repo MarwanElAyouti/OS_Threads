5cb1f053b9d279828426b5df38e5c3a3984d5040
MarwanElAyouti <marwan.hossam@gmail.com>
2019-12-17 01:12:43 +0100

    Flipping test with no threads

--- flip.c
+++ flip.c
@@ -23,20 +23,70 @@
 #include "flip.h"
 
 
+// Toggle bit n in v (by Robert Paauw)
+#define BIT_TOGGLE(v,n)      ((v) =  (v) ^ BITMASK(n))
+
 //Mutex declaration and initialization.
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+typedef struct 
+{
+    pthread_t id;
+    int index;   
+    int multiple;
+    bool active; 
+    bool done;
+} thread;
+
+static thread threads[NROF_THREADS]; 
+
+
+void intializeBuffer() {
+	for (int i = 0; i < sizeof(buffer); ++i)
+	{
+		buffer[i] = UINT128(0,0);
+	}
+}
+void printResult() 
+{
+	for (int i = 0; i < NROF_PIECES; ++i)
+	{
+		if(BIT_IS_SET(buffer[i / 128], i % 128))
+		{
+			printf("%d\n", i);
+		}
+	}
+}
+
+static void * flipPieces(void * arg)
+{
+	int32_t multiple = * (int *) arg;
+	free(arg);
+
+	for (int i = multiple; i <= NROF_PIECES; i += multiple)
+	{
+		BIT_TOGGLE(buffer[i / 128], i % 128)
+	}
+	
+}
 int main (void)
 {
-    // TODO: start threads to flip the pieces and output the results
-    // (see thread_test() and thread_mutex_test() how to use threads and mutexes,
-    //  see bit_test() how to manipulate bits in a large integer)
+    int multiple = 2;
+    
+    intializeBuffer();
+   	
+   	while(multiple <= NROF_PIECES)
+   		{
+   			int * multiple_arg = malloc(sizeof (int));
+   			*multiple_arg = multiple;
 
-	uint128_t v;
-	v = ~0;	
-	for (int i = 0; i <= sizeof(buffer); i++) {
-		buffer[i] = v;
-	}
-	printf ("buffer[0] (all 1's) : %lx%016lx\n", HI(buffer[0]), LO(buffer[0])); 
+   			flipPieces(&multiple_arg);
+
+   			multiple++;
+   		}
+
+   	printResult();
+
+
     return (0);
 }
